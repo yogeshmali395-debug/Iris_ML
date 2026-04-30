ac195cfadfa74162474687617ae1dee1af801e85
yogeshmali395-debug <yogeshmali395@gmail.com>
2026-04-30 17:53:01 +0530

    Update yog1.py

--- yog1.py
+++ yog1.py
@@ -7,17 +7,17 @@ op = input("Enter operation (+, -, *, /): ")
 
 # calculation logic
 if op == "+":
-    print("Result:", num1 + num2)
+    print("Result of Addition:", num1 + num2)
 
 elif op == "-":
-    print("Result:", num1 - num2)
+    print("Result of Substraction:", num1 - num2)
 
 elif op == "*":
-    print("Result:", num1 * num2)
+    print("Result of Multiplication:", num1 * num2)
 
 elif op == "/":
     if num2 != 0:
-        print("Result:", num1 / num2)
+        print("Result of Devide:", num1 / num2)
     else:
         print("Error: Division by zero")
 
